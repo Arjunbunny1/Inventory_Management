3b365f156884651a33577fd0daec8d33558870d8
Arjunbunny1 <gargarjun907@gmail.com>
2025-07-24 13:44:29 +0530

    Product model created

---
+++ models/Product.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  sku: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image_url: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
